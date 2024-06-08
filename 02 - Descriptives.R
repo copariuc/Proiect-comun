e5b8acaa7e477e2401071dfa2862b0aefb64ea7e
Cristian Opariuc-Dan <copariuc@gmail.com>
2024-06-08 18:32:47 +0300

    Analize descriptive univartiate

--- 02 - Descriptives.R
+++ 02 - Descriptives.R
@@ -1,7 +1,11 @@
 # Installing and loading libraries
 if(!require(rstatix)) install.packages("rstatix")
 if(!require(psych)) install.packages("psych")
-library(rstatix); library(psych)
+if(!require(sasLM)) install.packages("sasLM")
+if(!require(Hmisc)) install.packages("Hmisc")
+if(!require(PerformanceAnalytics)) install.packages("PerformanceAnalytics")
+library(rstatix); library(psych); library(sasLM); library(Hmisc)
+library(PerformanceAnalytics)
 
 # Loading dataset
 load(file = "NIAD.RData"); names(ds)
@@ -16,13 +20,33 @@ boxplot(ds$iq, horizontal = T, col = "green", outline = T)
 
 # II. Missing cases analysis and multiple imputation ####
 
-
 # III. Univariate statistics and normality ####
 #desc.age <- psych::describe(x = ds$varsta); desc.age
 #desc.iq <- psych::describe(x = ds$iq); desc.iq
 desc <- psych::describe(x = ds$varsta)
 desc <- rbind(desc,  psych::describe(x = ds$iq))
 rownames(desc) <- c("Age", "IQ"); desc
+SkewnessSE(ds$varsta); KurtosisSE(ds$varsta)
+## Stabilim intervalul de incredere (+/- 2ES)
+2 * SkewnessSE(ds$varsta); 2 * KurtosisSE(ds$varsta)
+# CI 95% Skweness: -0.6548913 .....0..... +0.6548913
+# CI 95% Kurtosis: -1.288839 ......0..... +1.288839
+## Verificam daca Skewness (Kurtosis) intra sau nu in intervalul de incredere
+## Varsta S=0.15, Kurtosis=-1.16
+
+## Skewness iese din interval la dreapta - Asimetrica pozitiv (scoruri accentuate mari)
+## Skewness iese din interval la stanga - Asimetrica negativ (scoruri accentuate mici)
+## Skewness intra in interval - Simetrica
+
+## Kurtosis iese din interval la dreapta - Leptocurtica (ascutita, variabilitate mica)
+## Kurtosis iese din interval la stanga - Platicurtica (turtita, variabilitate mare)
+## Kurtosis intra in interval - Mezocurtica
+
+# 2 * SkewnessSE(ds$iq); 2 * KurtosisSE(ds$iq)
+
+# Teste de normalitate
+shapiro.test(ds$varsta) # Asumptia normalitatii univariate NU ESTE indeplinita
+shapiro.test(ds$iq)     # Asumptia normalitatii univariate ESTE indeplinita
 
 # IV. Participants' characteristics ####
 n <- nrow(ds)
@@ -35,7 +59,7 @@ gen.p <- round(gen / n * 100, 2); gen.p
 
 
 # VI. Correlation matrix ####
-
-
-
+ds.cor <- as.matrix(ds[, c("varsta", "iq")]); head(ds.cor)
+rcorr(ds.cor, type = "spearman")
+chart.Correlation(R = ds.cor, histogram = T, method = "spearman")
 
